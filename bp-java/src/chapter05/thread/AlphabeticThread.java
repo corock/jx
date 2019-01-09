24e7c73b44c2b478712df20f15c8907e1b7c85f4
CoRock <dycorphin@gmail.com>
2019-01-09 15:01:19 +0900

    Add thread example 2: extending Thread

---
+++ bp-java/src/chapter05/thread/AlphabeticThread.java
@@ -0,0 +1,18 @@
+package chapter05.thread;
+
+public class AlphabeticThread extends Thread {
+
+	@Override
+	public void run() {
+		for (char c = 'a'; c <= 'z'; c++) {
+			System.out.print(c);
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
