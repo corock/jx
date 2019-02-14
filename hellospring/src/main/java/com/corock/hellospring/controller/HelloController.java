355cdcd004191e8909f2dcd0a6e9049b75c908e2
CoRock <dycorphin@gmail.com>
2019-02-14 17:11:17 +0900

    Create the dynamic web project
    1. Convert to maven project
    2. Set dependencies to pom.xml
    3. Set servlet settings to web.xml
    4. Add spring-servlet.xml
    5. Test a HelloController

---
+++ hellospring/src/main/java/com/corock/hellospring/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.corock.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+	// #1. @ResponseBody
+	@RequestMapping("/hello")
+	public String hello() {
+		// #1. return "Hello Spring!";
+		return "/WEB-INF/views/hello.jsp";
+	}
+	
+}
