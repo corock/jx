78c68beece231bc9ca6e8f3eda6477395cd9b73d
CoRock <dycorphin@gmail.com>
2019-02-26 11:16:28 +0900

    Add StopWatch to log execution time

--- mysite3/src/main/java/com/corock/mysite/repository/UserDAO.java
+++ mysite3/src/main/java/com/corock/mysite/repository/UserDAO.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StopWatch;
 
 import com.corock.mysite.exception.UserDAOException;
 import com.corock.mysite.vo.UserVO;
@@ -24,16 +25,27 @@ public class UserDAO {
 		return sqlSession.selectOne( "user.getByNo", no );
 	}
 	
-	public UserVO get( String email ) {
+	public UserVO get( String email ) {		
 		return sqlSession.selectOne( "user.getByEmail", email );
 	}
 	
 	public UserVO get( String email, String password ) throws UserDAOException {
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
+		
 		Map<String, String> map = new HashMap<>();
 		map.put( "email", email );
 		map.put( "password", password );
+
+		UserVO userVo = sqlSession.selectOne( "user.getByEmailAndPassword", map ); 
 		
-		return sqlSession.selectOne( "user.getByEmailAndPassword", map );
+		stopWatch.stop();
+		Long totalTime = stopWatch.getTotalTimeMillis();
+		
+		// Working with logging
+		System.out.println("totalTime: " + totalTime);
+
+		return userVo;
 	}
 	
 	public int insert( UserVO vo ) {
