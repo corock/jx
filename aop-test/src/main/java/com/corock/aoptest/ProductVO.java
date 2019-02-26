663ae150ce0dfaa65d1d97ee20706fae3aaed62b
CoRock <dycorphin@gmail.com>
2019-02-26 12:32:22 +0900

    Add aspect oriented programming examples

---
+++ aop-test/src/main/java/com/corock/aoptest/ProductVO.java
@@ -0,0 +1,24 @@
+package com.corock.aoptest;
+
+public class ProductVO {
+
+	private String name;
+
+	public ProductVO(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "ProductVO [name=" + name + "]";
+	}
+
+}
