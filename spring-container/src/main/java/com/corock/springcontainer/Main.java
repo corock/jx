9aa358d63ed5db6d8734529d31a0f8f202182d14
CoRock <dycorphin@gmail.com>
2019-03-04 10:43:55 +0900

    Update applicationContext.xml
    bean settings with maven

--- spring-container/src/main/java/com/corock/springcontainer/Main.java
+++ spring-container/src/main/java/com/corock/springcontainer/Main.java
@@ -2,14 +2,36 @@ package com.corock.springcontainer;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
 public class Main {
 
 	public static void main(String[] args) {
-		testBeanFactory();
+//		testBeanFactory();
+		testApplicationContext();
 	}
 	
+	private static void testApplicationContext() {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("config/applicationContext.xml");
+		
+		User1 user1 = ac.getBean( User1.class );
+		System.out.println( user1.getName() );
+
+		// 오류: bean 설정은 id나 name 설정을 해야 한다.
+		// user = (User1) ac.getBean( "user1" );
+		// System.out.println( user.getName() );
+		
+		// name으로 가져오기
+		User user = (User) ac.getBean( "user" );
+		System.out.println( user.getName() );
+
+		// id로 가져오기
+		user = (User) ac.getBean( "usr" );
+		System.out.println( user.getName() );
+	}
+
 	public static void testBeanFactory() {
 		// @ 설정인 경우, id가 자동으로 만들어진다. ex) User1 => user1
 		BeanFactory bf1 = new XmlBeanFactory( new ClassPathResource("config/applicationContext2.xml") );
