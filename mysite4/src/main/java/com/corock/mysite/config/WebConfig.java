58831072b908a4171d75c4cf699005ce9a0f6340
CoRock <dycorphin@gmail.com>
2019-03-21 16:32:53 +0900

    Add configuration mvc
    1. Get it right AppConfig.java
    2. Add import annotation to WebConfig
    3. Copy api controller package

---
+++ mysite4/src/main/java/com/corock/mysite/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.corock.mysite.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import com.corock.config.web.FileUploadConfig;
+import com.corock.config.web.MVCConfig;
+import com.corock.config.web.MessagesConfig;
+import com.corock.config.web.SecurityConfig;
+
+/**
+ * @EnableWebMvc: <mvc:annotation-driven /> in spring-servlet.xml
+ * @ComponentScan: <context:component-scan> in applicationConext.xml
+ */
+@Configuration
+@ComponentScan(value = { "com.corock.mysite.controller", "com.corock.mysite.exception" })
+@Import( MVCConfig.class )
+public class WebConfig {
+}
